deea831f0f42e252b2878d422840bd99f8a31daa
NathanPRangel <nathanpk20@gmail.com>
2023-09-17 13:32:30 -0300

    versão 1

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,57 +1,50 @@
+import { usersServices } from "../services/users.services";
 import httpStatus from "http-status";
 import { Request, Response } from "express";
 import { User } from "../protocols/users.protocols";
 import { usersRepository } from "../repository/users.repository";
 import { errors } from "../erros/erros";
 
-import { usersServices } from "../services/users.services";
 
 export async function postUser(req: Request, res: Response) {
-  try {
+
     const { name, email } = req.body as User;
+
     await usersServices.postUser(name, email);
+
     res.sendStatus(httpStatus.CREATED);
-  } catch (error) {
-    console.error("Erro ao criar usuário:", error);
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).send("Erro ao criar usuário.");
-  }
+
 }
 
-export async function getUser(req: Request, res: Response) {
-  try {
+export async function getUser (req: Request, res: Response) {
+
     const users = await usersRepository.getUser();
-    res.status(httpStatus.OK).send(users.rows);
-  } catch (error) {
-    console.error("Erro ao buscar usuários:", error);
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).send("Erro ao buscar usuários.");
-  }
+
+    return res.status(httpStatus.OK).send(users.rows);
 }
 
-export async function deleteUser(req: Request, res: Response) {
-  try {
+export async function deleteUser (req: Request, res: Response) {
+
     const { id } = req.params;
+
     await usersRepository.deleteUser(id);
+
     res.sendStatus(httpStatus.NO_CONTENT);
-  } catch (error) {
-    console.error(`Erro ao excluir usuário com ID ${req.params.id}:`, error);
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).send(`Erro ao excluir usuário com ID ${req.params.id}.`);
-  }
 }
 
-export async function updateUser(req: Request, res: Response) {
-  try {
+export async function updateUser (req: Request, res: Response) {
+
     const { id } = req.params;
+
     const { name, email } = req.body as User;
-    
+
     const resultado = await usersRepository.existsUser(id);
-    if (resultado.rows.length === 0) {
-      throw errors.notFound("Esse usuário");
-    }
+
+    if (resultado.rows.length === 0) throw errors.notFound("Esse usuário")
+
+   await usersRepository.updateUser(name, email, id);
+
     
-    await usersRepository.updateUser(name, email, id);
+
     res.sendStatus(httpStatus.NO_CONTENT);
-  } catch (error) {
-    console.error(`Erro ao atualizar usuário com ID ${req.params.id}:`, error);
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).send(`Erro ao atualizar usuário com ID ${req.params.id}.`);
-  }
 }
